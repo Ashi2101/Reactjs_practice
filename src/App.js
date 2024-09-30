1098e348ed0854f88051dabdfc79587065059554
Vivek Patel <vivek.patel371@gmail.com>
2024-09-30 20:26:26 +0530

    Issues Solved

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import './App.css';
 import NavBar from './components/NavBar';
 
-//import Table from './components/Table';
-//import Home from './components/pages/Home';
-//import About from './components/pages/About';
-//import Contact from './components/pages/Contact';
-
 import Form from './components/Form';
 
 function App() {
   return (
     <div>
-      <NavBar/>
-     
-      
+     <NavBar/>  
+     <Form/>
     </div>
 
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
-export default function NavBar() {
-
-        return 
-            <div>
-                <nav className="navbar navbar-expand-lg bg-body-tertiary">
+const NavBar = () => {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
                     <div className="container-fluid">
                         <a className="navbar-brand" href="#">Navbar</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -40,9 +39,9 @@ export default function NavBar() {
                         </div>
                     </div>
                 </nav>
-            </div>;
-        
-    }
-
-
-
+      </div>
+    )
+  }
+  
+  export default NavBar
+  
